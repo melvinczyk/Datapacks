490ee159f923357859d82fd75de7f7457abdf6a3
Nick Burczyk <nburczyk@icloud.com>
2025-01-19 15:32:06 -0600

    More bosses and planes complete

---
+++ Server Season 3/increment.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+def update_gunnite_count(json_data):
+    for material in json_data.get("materials", []):
+        if material.get("item") == "jeg:gunnite_ingot":
+            material["count"] += 1
+    return json_data
+
+def process_json_files(folder_path):
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".json"):
+            file_path = os.path.join(folder_path, filename)
+            
+            with open(file_path, "r") as file:
+                json_data = json.load(file)
+            
+            updated_json = update_gunnite_count(json_data)
+            
+            with open(file_path, "w") as file:
+                json.dump(updated_json, file, indent=2)
+
+folder_path = "/Users/nicholasburczyk/Desktop/Minecraft server stuff/Datapacks/Server Season 3/Aurora's-Arsenal/data/aurorasarsenal/recipes"
+process_json_files(folder_path)
